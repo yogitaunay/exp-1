469ce83e539a26deb47a9e0d4d0cc87863ac99c4
Prayogi Taunay <yogitaunay@gmail.com>
2019-08-12 19:00:17 +0700

    Prettier header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import '../../styles/header.scss';
+import "../../styles/header.scss";
 
 class Header extends Component {
-  
   render() {
     return (
       <>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <a class="navbar-brand" href="#">
-            <img src="../../favicon.ico" width="30" height="30" class="d-inline-block align-top" alt="" />
-            {' Toyota Semarang'}
+            <img
+              src="../../favicon.ico"
+              width="30"
+              height="30"
+              class="d-inline-block align-top"
+              alt=""
+            />
+            {" Toyota Semarang"}
           </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="modal" data-target=".bd-example-modal-lg">Product</a>
+                <a
+                  class="nav-link"
+                  href="#"
+                  data-toggle="modal"
+                  data-target=".bd-example-modal-lg"
+                >
+                  Product
+                </a>
 
-                <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div
+                  class="modal fade bd-example-modal-lg"
+                  tabindex="-1"
+                  role="dialog"
+                  aria-labelledby="myLargeModalLabel"
+                  aria-hidden="true"
+                >
                   <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                       {/* --------------Start Accordion--------------- */}
@@ -30,46 +58,106 @@ class Header extends Component {
                         <div class="card">
                           <div class="card-header" id="headingOne">
                             <h5 class="mb-0">
-                              <button class="btn" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              <button
+                                class="btn"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#collapseOne"
+                                aria-expanded="true"
+                                aria-controls="collapseOne"
+                              >
                                 Product MPV
                               </button>
                             </h5>
                           </div>
-                          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                          <div
+                            id="collapseOne"
+                            class="collapse show"
+                            aria-labelledby="headingOne"
+                            data-parent="#accordionExample"
+                          >
                             <div class="card-body">
                               <div class="row">
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/allnewavanza_q8uFzFblP.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/allnewavanza_q8uFzFblP.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                   <h6 class="text-left">All New Avanza</h6>
                                 </div>
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/newventurer_c_U3a5a6Y.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/newventurer_c_U3a5a6Y.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                   <h6 class="text-left">New Venturer</h6>
                                 </div>
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/veloz_wOlLZpVMi.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/veloz_wOlLZpVMi.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                   <h6 class="text-left">Avanza Veloz</h6>
                                 </div>
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/Alphard_eJDblFot8.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/Alphard_eJDblFot8.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                   <h6 class="text-left">Alphard</h6>
                                 </div>
                               </div>
                               <div class="row">
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/Kijang_Innova_asxPAP3jt.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/Kijang_Innova_asxPAP3jt.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                   <h6 class="text-left">Kijang Innova</h6>
                                 </div>
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/newventurer_c_U3a5a6Y.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/newventurer_c_U3a5a6Y.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                   <h6 class="text-left">New Venturer</h6>
                                 </div>
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/veloz_wOlLZpVMi.png" width="250" height="150" class="d-inline-block align-top" alt="" />
-                                  <h6 class="text-left">Avanza Veloz</h6>
+                                  <img
+                                    src="/images/avanza.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
+                                  <h6 class="text-left">Cobaakk nih</h6>
                                 </div>
                                 <div class="col">
-                                  <img src="https://ik.imagekit.io/nasmoco/allnewavanza_q8uFzFblP.png" width="250" height="150" class="d-inline-block align-top" alt="" />
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/allnewavanza_q8uFzFblP.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
                                 </div>
                               </div>
                             </div>
@@ -78,28 +166,76 @@ class Header extends Component {
                         <div class="card">
                           <div class="card-header" id="headingTwo">
                             <h5 class="mb-0">
-                              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                              <button
+                                class="btn btn-link collapsed"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#collapseTwo"
+                                aria-expanded="false"
+                                aria-controls="collapseTwo"
+                              >
                                 Collapsible Group Item #2
                               </button>
                             </h5>
                           </div>
-                          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                          <div
+                            id="collapseTwo"
+                            class="collapse"
+                            aria-labelledby="headingTwo"
+                            data-parent="#accordionExample"
+                          >
                             <div class="card-body">
-                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                              Anim pariatur cliche reprehenderit, enim eiusmod
+                              high life accusamus terry richardson ad squid. 3
+                              wolf moon officia aute, non cupidatat skateboard
+                              dolor brunch. Food truck quinoa nesciunt laborum
+                              eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+                              put a bird on it squid single-origin coffee nulla
+                              assumenda shoreditch et. Nihil anim keffiyeh
+                              helvetica, craft beer labore wes anderson cred
+                              nesciunt sapiente ea proident. Ad vegan excepteur
+                              butcher vice lomo. Leggings occaecat craft beer
+                              farm-to-table, raw denim aesthetic synth nesciunt
+                              you probably haven't heard of them accusamus
+                              labore sustainable VHS.
                             </div>
                           </div>
                         </div>
                         <div class="card">
                           <div class="card-header" id="headingThree">
                             <h5 class="mb-0">
-                              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                              <button
+                                class="btn btn-link collapsed"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#collapseThree"
+                                aria-expanded="false"
+                                aria-controls="collapseThree"
+                              >
                                 Collapsible Group Item #3
                               </button>
                             </h5>
                           </div>
-                          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                          <div
+                            id="collapseThree"
+                            class="collapse"
+                            aria-labelledby="headingThree"
+                            data-parent="#accordionExample"
+                          >
                             <div class="card-body">
-                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                              Anim pariatur cliche reprehenderit, enim eiusmod
+                              high life accusamus terry richardson ad squid. 3
+                              wolf moon officia aute, non cupidatat skateboard
+                              dolor brunch. Food truck quinoa nesciunt laborum
+                              eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+                              put a bird on it squid single-origin coffee nulla
+                              assumenda shoreditch et. Nihil anim keffiyeh
+                              helvetica, craft beer labore wes anderson cred
+                              nesciunt sapiente ea proident. Ad vegan excepteur
+                              butcher vice lomo. Leggings occaecat craft beer
+                              farm-to-table, raw denim aesthetic synth nesciunt
+                              you probably haven't heard of them accusamus
+                              labore sustainable VHS.
                             </div>
                           </div>
                         </div>
@@ -110,19 +246,21 @@ class Header extends Component {
                 </div>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Price List</a>
+                <a class="nav-link" href="#">
+                  Price List
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Promo</a>
+                <a class="nav-link" href="#">
+                  Promo
+                </a>
               </li>
             </ul>
           </div>
         </nav>
       </>
     );
-
   }
-  
 }
 
 export default Header;
