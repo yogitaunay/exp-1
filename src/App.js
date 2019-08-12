d11457e57519ebd5176efc47a2023bef793af228
yogitaunay <yogitaunay@gmail.com>
2019-08-12 08:40:51 +0700

    4th commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/layout/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+
+    </div>
+  );
+}
+
+export default App;
