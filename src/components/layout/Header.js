5ba2e3c9b734dddc9eba6b633242494f89a71166
Prayogi Taunay <yogitaunay@gmail.com>
2019-08-18 12:31:51 +0700

    Completing the Menu Modal

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../../styles/header.scss";
 
 class Header extends Component {
@@ -59,14 +59,14 @@ class Header extends Component {
                           <div class="card-header" id="headingOne">
                             <h5 class="mb-0">
                               <button
-                                class="btn btn-light"
+                                class="btn btn-block btn-primary"
                                 type="button"
                                 data-toggle="collapse"
                                 data-target="#collapseOne"
                                 aria-expanded="true"
                                 aria-controls="collapseOne"
                               >
-                                Product MPV
+                                <b>MPV</b>
                               </button>
                             </h5>
                           </div>
@@ -178,14 +178,14 @@ class Header extends Component {
                           <div class="card-header" id="headingTwo">
                             <h5 class="mb-0">
                               <button
-                                class="btn collapsed btn-light"
+                                class="btn collapsed btn-block btn-secondary"
                                 type="button"
                                 data-toggle="collapse"
                                 data-target="#collapseTwo"
                                 aria-expanded="false"
                                 aria-controls="collapseTwo"
                               >
-                                Product SUV
+                                <b>SUV</b>
                               </button>
                             </h5>
                           </div>
@@ -244,15 +244,15 @@ class Header extends Component {
                         <div class="card">
                           <div class="card-header" id="headingThree">
                             <h5 class="mb-0">
-                              <button
-                                class="btn collapsed btn-light"
+                            <button
                                 type="button"
+                                class="btn btn-warning btn-block"
                                 data-toggle="collapse"
                                 data-target="#collapseThree"
                                 aria-expanded="false"
                                 aria-controls="collapseThree"
                               >
-                                Product Hatchback
+                                <b>HATCHBACK</b>
                               </button>
                             </h5>
                           </div>
@@ -263,20 +263,28 @@ class Header extends Component {
                             data-parent="#accordionExample"
                           >
                             <div class="card-body">
-                              Anim pariatur cliche reprehenderit, enim
-                              eiusmod high life accusamus terry richardson
-                              ad squid. 3 wolf moon officia aute, non
-                              cupidatat skateboard dolor brunch. Food truck
-                              quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-                              moon tempor, sunt aliqua put a bird on it
-                              squid single-origin coffee nulla assumenda
-                              shoreditch et. Nihil anim keffiyeh helvetica,
-                              craft beer labore wes anderson cred nesciunt
-                              sapiente ea proident. Ad vegan excepteur
-                              butcher vice lomo. Leggings occaecat craft
-                              beer farm-to-table, raw denim aesthetic synth
-                              nesciunt you probably haven't heard of them
-                              accusamus labore sustainable VHS.
+                              <div class="row">
+                                <div class="col">
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/agya/toyota-semarang-agya.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
+                                  <h6 class="text-left">New Agya</h6>
+                                </div>
+                                <div class="col">
+                                  <img
+                                    src="https://ik.imagekit.io/nasmoco/Yaris/toyota-semarang-yaris.png"
+                                    width="250"
+                                    height="150"
+                                    class="d-inline-block align-top"
+                                    alt=""
+                                  />
+                                  <h6 class="text-left">New Yaris</h6>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -284,14 +292,10 @@ class Header extends Component {
                           <div class="card-header" id="headingFour">
                             <h2 class="mb-0">
                               <button
-                                class="btn btn-light"
                                 type="button"
-                                data-toggle="collapse"
-                                data-target="#collapseFour"
-                                aria-expanded="true"
-                                aria-controls="collapseFour"
+                                class="btn btn-danger btn-block"
                               >
-                                Product Sedan
+                                <b>SEDAN</b>
                               </button>
                             </h2>
                           </div>
